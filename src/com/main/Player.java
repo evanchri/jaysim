f9e7469678a6dbbd7d5b98dce549b76cbc36e5ea
evanchri <evanchri@oregonstate.edu>
2019-09-11 16:57:45 -0400

    Initial Commit

---
+++ src/com/main/Player.java
@@ -0,0 +1,28 @@
+package com.main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Player extends GameObject
+{
+
+	public Player(int x, int y, ID id) 
+	{
+		super(x, y, id);
+		
+	}
+	
+	public void tick() 
+	{
+		
+	}
+	
+	public void render(Graphics g) 
+	{
+	
+		g.setColor(Color.white);
+		g.fillRect(x, y, 32, 32);
+		
+	}
+	
+}
